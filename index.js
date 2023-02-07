9dd2ff02cf896a2ac705c86847499e4c94aa0e91
Brian Nicholas King <bnk@Brians-MacBook-Pro.local>
2023-02-07 16:58:47 -0500

    scraping title and url okay, but price in separate div is an issue and needs work

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const PORT = 8000
-const axios = require('axios')
-const cheerio = require('cheerio')
-const { response } = require('express')
-const express = require('express')
+const PORT = 8000;
+const axios = require("axios");
+const cheerio = require("cheerio");
+const { response } = require("express");
+const express = require("express");
 
-const app = express()
-const url = 'https://www.sportsmans.com/'
+const app = express();
+const url =
+  "https://www.sportsmans.com/shooting-gear-gun-supplies/handguns/c/cat139633";
 
-axios(url)
-    .then(response => {
-        const html = response.data
-        console.log(html)
-    })
+axios(url).then((response) => {
+  const html = response.data;
+  const $ = cheerio.load(html);
+  const article = [];
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+  $(".details", html).each(function () {
+    const title = $(this).text().trim();
+    const url = $(this).find("a").attr("href");
+
+    article.push({
+      title,
+      url,
+    });
+  });
+  console.log(article);
+});
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
